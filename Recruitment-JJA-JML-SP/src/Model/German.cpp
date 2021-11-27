bcb64cfb87310312d86a3220fa9a8fde6edf65c2
jmlopezriani <jmlopezriani@javerianacali.edu.co>
2021-11-27 14:13:04 -0500

    Se actualiza diagrama

--- Recruitment-JJA-JML-SP/src/Model/German.cpp
+++ Recruitment-JJA-JML-SP/src/Model/German.cpp
@@ -13,13 +13,13 @@ German::German() {
 string German::culturalInfo() {
     return name + "\n"
                   "Type: Reserved culture.\n"
-                  "EyeContact: \n"
-                  "Gestures: Yes: No:  Good Luck:\n"
-                  "Touch: \n"
-                  "Personal space: \n"
-                  "Time management: \n"
-                  "Communication style: \n"
-                  "Nap: ";
+                  "EyeContact: Constant direct eye contact is expected.\n"
+                  "Gestures: Yes: Touch index with thumb, No: Thumbs down, OK: same as yes, Good Luck: Wrapping fingers around thumbs.\n"
+                  "Touch: Don't touch unless you are good friends with the person.\n"
+                  "Personal space: Always keep at an arm's length.\n"
+                  "Time management: Very punctual, they plan ahead of time and stick to it.\n"
+                  "Communication style: High volume, normal rhythm, intense tone.\n"
+                  "Nap: no naps.\n";
 
 }
 
--- Recruitment-JJA-JML-SP/src/Model/Japanese.cpp
+++ Recruitment-JJA-JML-SP/src/Model/Japanese.cpp
@@ -13,11 +13,11 @@ Japanese::Japanese() {
 string Japanese::culturalInfo() {
     return name + "\n"
                   "Type: Reserved culture.\n"
-                  "EyeContact: \n"
+                  "EyeContact: Indirect eye contact. Direct eye contact is interpreted as intimidation.\n"
                   "Gestures: Yes: No:  Good Luck:\n"
-                  "Touch: \n"
-                  "Personal space: \n"
-                  "Time management: \n"
+                  "Touch: Avoid touch unless you're good friends with the person. Greet with a bow, no handshake.\n"
+                  "Personal space: If possible, keep appropriate distance between one another.\n"
+                  "Time management: Very punctual, they plan ahead of time and stick to it.\n"
                   "Communication style: \n"
                   "Nap: ";
 
